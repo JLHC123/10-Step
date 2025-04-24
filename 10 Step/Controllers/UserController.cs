ad3c0bcd3a2981fe94ff9da651b24687338ccf23
Jorge Hernandez Cruz <jlhc3751@gmail.com>
2025-04-24 11:29:23 -0700

    Removed WeatherForecast related things and tested if the rest of the code works

--- 10 Step/Controllers/UserController.cs
+++ 10 Step/Controllers/UserController.cs
@@ -9,32 +9,8 @@ namespace _10_Step.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class UserController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-
         [HttpGet("Hello World")]
         public IActionResult HelloWorld()
         {
